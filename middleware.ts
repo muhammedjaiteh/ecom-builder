54db454d4b376dee7caeb0b0998e19cb363afa66
Gambia Builder <builder@gambia.com>
2026-01-31 11:09:47 +0000

    Fix middleware version

---
+++ middleware.ts
@@ -0,0 +1,26 @@
+import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs';
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export async function middleware(req: NextRequest) {
+  const res = NextResponse.next();
+  const supabase = createMiddlewareClient({ req, res });
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  // If user is NOT logged in AND tries to visit Dashboard...
+  if (!session && req.nextUrl.pathname.startsWith('/dashboard')) {
+    // ...Kick them back to Login!
+    const redirectUrl = req.nextUrl.clone();
+    redirectUrl.pathname = '/login';
+    return NextResponse.redirect(redirectUrl);
+  }
+
+  return res;
+}
+
+export const config = {
+  matcher: ['/dashboard/:path*'],
+};
